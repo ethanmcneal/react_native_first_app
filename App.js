37d680c60d10a359e9dd0c906161a0e231e6d0dc
ethan.mcneal <ethan.mcneal@gmail.com>
2021-05-26 15:51:31 -0600

    inline styline, lesson 24

--- App.js
+++ App.js
@@ -1,23 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  const [text, setText] = useState("YOOOO Open up App.js to start working on your app!")
+  // const [text, setText] = useState("YOOOO Open up App.js to start working on your app!")
   return (
-    <View style={styles.container}>
-      <Text>{text}</Text>
-      <Button title="Change Text" onPress={() => setText('The Text Changed!!')}/>
-      <StatusBar style="auto" />
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+      <TextInput placeholder='Course Goal' style={{borderColor: 'black', borderWidth: 1, padding: 10, width: '80%'}}/>
+      <Button title='Add'/>
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
