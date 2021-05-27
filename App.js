284ab021465ea6e64614db1ba1efe8c0cc5c5c2f
ethan.mcneal <ethan.mcneal@gmail.com>
2021-05-27 13:53:26 -0600

    scroll view

--- App.js
+++ App.js
@@ -1,19 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  // const [text, setText] = useState("YOOOO Open up App.js to start working on your app!")
-  return (
-    <View style={{padding: 50}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-      <TextInput placeholder='Course Goal' style={{borderColor: 'black', borderWidth: 1, padding: 10, width: '80%'}}/>
-      <Button title='Add'/>
-      </View>
-      <View>
+  const [enteredGoal, setEnteredGoal] = useState("")
+  const [courseGoals, setCourseGoals] = useState([])
 
+  const change = (enteredText) => {
+    setEnteredGoal(enteredText)
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...courseGoals, enteredGoal])
+    setEnteredGoal("")
+  }
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={change}
+          value={enteredGoal}
+        />
+        <Button title="Add" onPress={addGoalHandler}/>
       </View>
+      <ScrollView>
+        {courseGoals.map((goal, index)=> <View key={index} style={styles.listItem}>
+          <Text >
+            {goal}
+          </Text>
+        </View>)}
+      </ScrollView>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    width: "80%",
+  },
+  listItem : {
+    padding: 8,
+    backgroundColor: '#888',
+    margin: 2,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 8,
+  }
+});
